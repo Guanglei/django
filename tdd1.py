ca5fdd49ff092531a869ef7ba7fff7742e9d370a
guanglei <guanglei@localhost.localdomain>
2014-08-22 00:13:16 +0800

    update testcase

--- tdd1.py
+++ tdd1.py
@@ -1,8 +1,29 @@
 #! /usr/bin/python
 
 from selenium import webdriver
+import unittest
 
+"""
 browser = webdriver.Firefox()
 browser.get("http://localhost:8000")
 
-assert 'Django' in browser.title
+assert 'To-Do' in browser.title
+
+browser.quit()
+"""
+
+class NewVisitorTest(unittest.TestCase):
+  def setUp(self):
+    self.browser = webdriver.Firefox()
+
+  def tearDown(self):
+    self.browser.quit()
+
+  def test_can_start_a_list_and_retrieve_it_later(self):
+    self.browser.get("http://localhost:8000")
+
+    self.assertTrue('To-Do' in self.browser.title)
+    self.fail("Finished the test!")
+
+if __name__ == "__main__":
+  unittest.main()
